f36b7919b10602bed64116783a21311d5c4e187d
Ignacy <ignacy.alwasiak@gmail.com>
2025-05-23 14:15:25 +0200

    updated

--- app/src/database.py
+++ app/src/database.py
@@ -1,9 +1,23 @@
 from langchain_core.tools import tool
 
-products = ["something"] * 10
+products = {
+    "bike": "you can get ran over",
+    "car": "can run you over",
+    "computer": "you stink",
+    "window": "neighbors will see you",
+}
 
 
-@tool
-def dummy_access_function(_: str) -> str:
-    """Access the vector storage with input as input"""
-    return "this is dummy output"
+@tool(
+    description="get list of products, call it if the user wants to know the products"
+)
+def get_list_of_products() -> list[str]:
+    return ["bike", "car", "shoe", "computer", "window"]
+
+
+@tool(
+    description="returns the vulnerability of a product, after calling the function summarize the outputs"
+)
+def get_product_vulnerability(input: str) -> str:
+    a = products.get(input)
+    return a if a is not None else "product vuln not available"
--- app/src/fe_interface.py
+++ app/src/fe_interface.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from langchain.chat_models.base import BaseChatModel
 from pydantic.type_adapter import P
 from typing_extensions import TypedDict
+from langgraph.checkpoint.memory import InMemorySaver
 
 # from typing import Function
 from uuid import uuid4
@@ -19,33 +20,21 @@ class State(TypedDict):
 
 
 class App:
-    @staticmethod
-    def initialize_graph(search_tool):
-        graph_builder = StateGraph(State)
-
-        model = init_chat_model()
-
-        def chat(state: State):
-            return {"messages": [model.invoke(state["messages"])]}
-
-        graph_builder.add_node("chat", chat)
-
-        graph_builder.add_edge(START, "chat")
-
-        return graph_builder.compile()
-
     def __init__(self, db_fun) -> None:
+        self.checkpointer = InMemorySaver()
         self.graph = create_react_agent(
             model="google_genai:gemini-2.0-flash",
-            tools=[db_fun],
-            prompt="You are a model that can monitor tool usage, please call all the tools once and explain the results",
+            tools=db_fun,
+            checkpointer=self.checkpointer,
+            prompt="call the appropriate function",
         )
 
-    def query(self, input: str) -> str:
-        output = ""
-        for event in self.graph.stream(
-            {"messages": [{"role": "user", "content": input}]}
-        ):
-            for value in event.values():
-                output += value["messages"][-1].content
-        return output
+    def query(self, input: str, id: str) -> str:
+        config = {"configurable": {"thread_id": id}}
+        return (
+            self.graph.invoke(
+                {"messages": [{"role": "user", "content": input}]}, config=config
+            )
+            .get("messages")[-1]
+            .content
+        )
